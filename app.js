7c34076899997d38a3a67f90fa3ea901ae0e4ad4
Shourav Khan <102287391+Shourav-khan@users.noreply.github.com>
2025-01-20 18:47:59 +0600

    function

--- app.js
+++ app.js
@@ -512,4 +512,23 @@
 
 
 
+// let inputArray = [
+//   { id: 1, name: "Alice" },
+//   { id: 2, name: "Bob" },
+//   { id: 3, name: "Charlie" },
+// ];
+
+// const arrayToObj = (arr) => {
+//   //   console.log(arr[2].id);
+//   let obj = {};
+//   for (let key of arr) {
+//     console.log(key.id, key);
+//     obj[key.id] = key;
+//     // console.log(key);
+//   }
+//   return obj;
+// };
+
+// console.log(arrayToObj(inputArray));
+// // Should print: { '1': { id: 1, name: 'Alice' }, '2': { id: 2, name: 'Bob' }, '3': { id: 3, name: 'Charlie' } }
 
